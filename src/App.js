c50e7106834d42103904117bef76a109ff59e576
Boze-noob <bozeskoko16@gmail.com>
2022-09-21 18:25:38 +0200

    navBar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Home from './pages/home/Home';
+import NavBar from './components/ui/navBar/NavBar';
+import {theme} from './common/Theme';
+import {ThemeProvider} from '@mui/material/styles';
+
 function App() {
-  return null;
+  return (
+    <ThemeProvider theme={theme}>
+    <NavBar />
+    <Home />
+      
+    </ThemeProvider>
+  );
 }
 
 export default App;
