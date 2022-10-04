94f6cf95a4d2e76140cc35067b00c545d1f3c04b
Boze-noob <bozeskoko16@gmail.com>
2022-10-04 13:53:40 +0200

    refactored HorizontalImgList

--- src/components/ui/lists/HorizontalImgList.jsx
+++ src/components/ui/lists/HorizontalImgList.jsx
@@ -1,12 +1,12 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
-import { Paper, Button, Divider } from "@mui/material";
+import { Paper, Divider } from "@mui/material";
 
 export default function HorizontalImgList({ bgColor, title, fontFamily }) {
   var items = [
     {
       name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!",
+      description: "Random Name #1",
       imgUrl: [
         "https://www.whatsappimages.in/wp-content/uploads/2021/07/Top-HD-sad-quotes-for-whatsapp-status-in-hindi-Pics-Images-Download-Free.gif",
         "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
@@ -24,7 +24,7 @@ export default function HorizontalImgList({ bgColor, title, fontFamily }) {
     },
     {
       name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!",
+      description: "Random Name #1",
       imgUrl: [
         "https://www.whatsappimages.in/wp-content/uploads/2021/07/Top-HD-sad-quotes-for-whatsapp-status-in-hindi-Pics-Images-Download-Free.gif",
         "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
@@ -42,7 +42,7 @@ export default function HorizontalImgList({ bgColor, title, fontFamily }) {
     },
     {
       name: "Random Name #1",
-      description: "Probably the most random thing you have ever seen!",
+      description: "Random Name #1",
       imgUrl: [
         "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
         "https://siwi.org/wp-content/uploads/2021/07/why-water_topics-present-at-launch_rivers2.jpg",
@@ -90,6 +90,7 @@ export default function HorizontalImgList({ bgColor, title, fontFamily }) {
             color: "black",
           },
         }}
+        indicators={false}
       >
         {items.map((item, i) => (
           <Item key={i} item={item} />
@@ -102,31 +103,44 @@ export default function HorizontalImgList({ bgColor, title, fontFamily }) {
 function Item(props) {
   return (
     <Paper style={{ background: "transparent" }}>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        {props.item.imgUrl.map((item, i) => (
-          <Box
-            component="img"
-            sx={{
-              height: { xs: 140, sm: 340, md: 490 },
-              width: "30%",
-            }}
-            src={item}
-            style={{ objectFit: "fill", padding: 10 }}
-          />
-        ))}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Grid container spacing={2}>
+          {props.item.imgUrl.map((item, i) => (
+            <Grid item xs={4}>
+              <Box sx={{ flexDirection: "column" }}>
+                <Box
+                  component="img"
+                  sx={{
+                    height: { xs: 140, sm: 340, md: 490 },
+                    width: "100%",
+                  }}
+                  src={item}
+                  style={{ objectFit: "fill", padding: 10 }}
+                />
+                <Typography
+                  sx={{ mt: 5 }}
+                  style={{ textAlign: "center" }}
+                  variant="h6"
+                >
+                  {props.item.description}
+                </Typography>
+                <Typography
+                  sx={{ mt: 5 }}
+                  style={{ textAlign: "center", fontWeight: 600 }}
+                  variant="h6"
+                >
+                  {props.item.description}
+                </Typography>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
-      <Typography
-        sx={{ typography: { xs: "h6", sm: "h5" }, mt: 20 }}
-        style={{ textAlign: "center" }}
-      >
-        {props.item.description}
-      </Typography>
-      <Typography
-        sx={{ typography: { xs: "h6", sm: "h5" }, mt: 5 }}
-        style={{ textAlign: "center", fontWeight: 600 }}
-      >
-        {props.item.description}
-      </Typography>
     </Paper>
   );
 }
