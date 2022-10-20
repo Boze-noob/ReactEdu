4146ac0e251a3324649a6cab5c2c033dd5d222f5
Boze-noob <bozeskoko16@gmail.com>
2022-10-20 20:14:05 +0200

    about intro UI

--- src/pages/about/components/Intro.jsx
+++ src/pages/about/components/Intro.jsx
@@ -3,37 +3,53 @@ import { Box } from "@mui/system";
 
 export default function Intro() {
   return (
-    <Box width={"100%"} position={"relative"} bgcolor={"red"}>
-      <Typography top={"2%"} left={"5%"} position={"absolute"}>
-        {"DREAM \nBELIEVE \nACHIEVE"}
+    <Box width={"100%"} position={"relative"} height={"135vh"}>
+      <Typography
+        top={"10%"}
+        left={"10%"}
+        position={"absolute"}
+        variant="h3"
+        whiteSpace={"pre-wrap"}
+      >
+        {'"DREAM \n BELIEVE \n ACHIEVE."'}
       </Typography>
       <Image
+        top={"5%"}
+        right={"5%"}
         url={
           "https://sincerelyjules.com/wp-content/themes/sincerely/assets/img/pages/sj_about_image_01.png"
         }
-        top={"2%"}
-        right={"0%"}
-        width={{ xs: 100, sm: 200 }}
+        width={"60%"}
       />
+
       <Image
+        bottom={"5%"}
+        left={"10%"}
         url={
           "https://sincerelyjules.com/wp-content/themes/sincerely/assets/img/pages/sj_about_image_02.png"
         }
-        top={"20%"}
-        left={"0%"}
-        width={{ xs: 100, sm: 200 }}
+        width={"30%"}
       />
-      <Typography top={"20%"} left={"0%"} position={"absolute"}>
-        {
-          "HELLO! I am Julie (a.k.a Jules), welcome to my little world, Sincerely Jules, where you’ll see bits and pieces of my outfits, my fun travels, my favorite products, my beauty tips & tricks, parts of my home, sneak peaks of cool collabs, launches + more! Sincerely Jules is my happy place where I love to share parts of my life with you and bring you along for the ride! You have become my secondary family and I want you to feel like we are close friends and that I can share anything with you! It's a place where dreaming big is always encouraged and following your heart is too! I hope sharing with you parts of my life and journey, can somehow inspire you to live out your own dreams! It’s an open journal to you, I hope you enjoy it & it becomes your daily read because I always love having you come by. With love,Jules"
-        }
-      </Typography>
+
+      <Box
+        bottom={"10%"}
+        right={"1%"}
+        position={"absolute"}
+        sx={{ width: "55%", pr: "5%" }}
+      >
+        <Typography whiteSpace={"pre-wrap"}>
+          {
+            "HELLO! I am Julie (a.k.a Jules), welcome to my little world, Sincerely Jules, where you’ll see bits and pieces of my outfits, my fun travels, my favorite products, my beauty tips & tricks, parts of my home, sneak peaks of cool collabs, launches + more! Sincerely Jules is my happy place where I love to share parts of my life with you and bring you along for the ride! You have become my secondary family and I want you to feel like we are close friends and that I can share anything with you! It's a place where dreaming big is always encouraged and following your heart is too! I hope sharing with you parts of my life and journey, can somehow inspire you to live out your own dreams! It’s an open journal to you, I hope you enjoy it & it becomes your daily read because I always love having you come by. \n \n With love, \n Jules"
+          }
+        </Typography>
+      </Box>
+
       <Divider />
     </Box>
   );
 }
 
-function Image({ url, top, right, left, width }) {
+function Image({ url, top, right, bottom, left, width, height }) {
   return (
     <Box
       component="img"
@@ -41,10 +57,10 @@ function Image({ url, top, right, left, width }) {
       sx={{
         objectFit: "fill",
         width: width,
-        height: 300,
       }}
       top={top}
       right={right}
+      bottom={bottom}
       left={left}
       position={"absolute"}
     />
