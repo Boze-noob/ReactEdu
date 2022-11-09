5741f2f6b93d17960dfdafdcfc58b29f5658dc6a
Boze-noob <bozeskoko16@gmail.com>
2022-11-09 21:22:17 +0100

    fake data

---
+++ src/fakeData/shoppe.js
@@ -0,0 +1,1419 @@
+export const jewlery = [
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/07/IMG_3469.jpg",
+    title: "COMPLETEDWORKS",
+    description: "14k Gold-Plated Earrings",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/07/IMG_3472.jpg",
+    title: "WOLF CIRCUS",
+    description: "Marta Earrings",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/07/IMG_3471.jpg",
+    title: "WOLF CIRCUS",
+    description: "Layla Pearl Earrings",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/07/IMG_3470.jpg",
+    title: "CANO",
+    description: "Gamuke Earrings",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/04/IMG_0326.jpg",
+    title: "ELIOU",
+    description: "Deede Beaded Necklace",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/04/IMG_0329.jpg",
+    title: "BEN-AMUN",
+    description: "Button Earrings",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/04/IMG_0330.jpg",
+    title: "DANS LES RUES SRLS",
+    description: "Mama Ring",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/04/IMG_0328.jpg",
+    title: "BRINKER & ELIZA",
+    description: "Penny Bracelet",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/02/Sincerely_jules_jennifer-behr.jpg",
+    title: "JENNIFER BEHR",
+    description: "Monica Pearl and Bean Necklace",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/02/sincerely_jules_reggie.jpg",
+    title: "REGGIE",
+    description: "Gold-Plated Earrings",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/02/khiry_sincerely_jules.jpg",
+    title: "KHIRY",
+    description: "Khartoum Stacking Ring",
+  },
+  {
+    img: "https://sincerelyjules.com/wp-content/uploads/2022/02/sincerely-jules-1.jpg",
+    title: "BEN-AMUN",
+    description: "Hammered Gold-Plated Chain Bracelet",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const sunglasses = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const hairAccessories = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const hats = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const activeWear = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const dresses = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const tops = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const knits = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const pants = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const denim = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const shorts = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const skirts = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const swimwear = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const bags = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const boots = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const heels = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const flats = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const sneakers = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const sandals = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
+
+export const nailPolish = [
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+  {
+    img: "",
+    title: "",
+    description: "",
+  },
+];
