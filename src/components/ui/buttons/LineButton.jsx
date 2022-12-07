8e8556939c47e21319f47a7f7945f599822dba55
Boze-noob <bozeskoko16@gmail.com>
2022-12-07 20:20:12 +0100

    LineButton hover effect

--- src/components/ui/buttons/LineButton.jsx
+++ src/components/ui/buttons/LineButton.jsx
@@ -3,22 +3,38 @@ import { Typography, Divider, Box, Link } from "@mui/material";
 export default function LineButton({ btnTxt, width, url }) {
   return (
     <Link underline="none" href={url}>
-      <Box alignItems="center" sx={{ display: "flex", flexDirection: "row" }}>
+      <Box
+        alignItems="center"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          "&:hover": {
+            cursor: "pointer",
+            "& .divider": {
+              bgcolor: "black",
+            },
+            "& .typography": {
+              color: "black",
+            },
+          },
+          transition: "0.7s",
+        }}
+      >
         <Divider
+          className="divider"
           sx={{
             height: 2,
             width: width,
             bgcolor: "white",
+            transition: "0.7s",
           }}
         ></Divider>
         <Typography
+          className="typography"
           color={"white"}
           sx={{
             pl: 15,
             fontSize: { xs: 8, sm: 16 },
-            "&:hover": {
-              color: "black",
-            },
             transition: "0.7s",
           }}
           style={{
