d7b5755e5d10763ea3847136fbeaadab2cb8a4e3
Boze-noob <bozeskoko16@gmail.com>
2022-12-10 20:25:44 +0100

    added navigation for category

--- src/pages/fashion/Fashion.jsx
+++ src/pages/fashion/Fashion.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import PageTitle from "../../components/ui/header/PageTitle.jsx";
-import NavBar from "../../components/ui/navBar/NavBar.jsx";
+import { useEffect } from "react";
 import HorizontalArticleImgRight from "../../components/ui/articles/horizontalArticles/HorizontalArticleImgRight";
 import HorizontalArticleImgLeft from "../../components/ui/articles/horizontalArticles/HorizontalArticleImgLeft";
 import VerticalGalleryArticleList from "../../components/ui/lists/VerticalGalleryArticleList.jsx";
+import { useArticleStore } from "../../stores/ArticleStore.js";
+import { getArticles } from "../../data/repositories/ArticleRepository.js";
 
 export default function FashionPage() {
+  const articles = useArticleStore((state) => state.articles);
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
   return (
     <>
       <PageTitle title={"Fashion"} />
-      <HorizontalArticleImgLeft marginTop={60} />
-      <HorizontalArticleImgRight marginTop={-40} />
+      <HorizontalArticleImgLeft marginTop={60} article={articles[0]} />
+      <HorizontalArticleImgRight marginTop={-40} article={articles[0]} />
       <VerticalGalleryArticleList marginTop={40} loadMoreFlag={false} />
-      <HorizontalArticleImgLeft marginTop={60} />
-      <HorizontalArticleImgRight marginTop={-40} />
+      <HorizontalArticleImgLeft marginTop={60} article={articles[0]} />
+      <HorizontalArticleImgRight marginTop={-40} article={articles[0]} />
       <VerticalGalleryArticleList marginTop={40} loadMoreFlag={true} />
     </>
   );
