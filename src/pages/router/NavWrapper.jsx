fdb12225dc05c9a24fbe7f9935e7294f68cd835d
Boze-noob <bozeskoko16@gmail.com>
2022-12-27 21:52:16 +0100

    scroll to top btn added

--- src/pages/router/NavWrapper.jsx
+++ src/pages/router/NavWrapper.jsx
@@ -13,6 +13,28 @@ export const NavWrapper = () => {
         primaryColor={"black"}
         secondaryColor={location.pathname === "/" ? "white" : "black"}
       />
+      <button
+        onClick={() => {
+          window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+        }}
+        style={{
+          position: "fixed",
+          padding: "1rem 2rem",
+          fontSize: "20px",
+          bottom: "40px",
+          right: "40px",
+          backgroundColor: "#0C9",
+          color: "#fff",
+          textAlign: "center",
+          zIndex: 1,
+          "&:hover": {
+            cursor: "pointer",
+            bgcolor: "#333333",
+          },
+        }}
+      >
+        Scroll to top
+      </button>
       <Outlet></Outlet>
       <Footer marginTop={40} />
     </>
